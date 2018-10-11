fbd5067ea88d532635c1578fb5af241da335b0b4
Urny97 <arnebackx@hotmail.com>
2018-10-11 20:28:45 +0200

    added files

---
+++ AES128.vhd
@@ -0,0 +1,137 @@
+library ieee;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity AES128 is
+  port(
+    reset: in std_logic;
+    clock: in std_logic;
+    ce: in std_logic;
+    data_in: in std_logic_vector(127 downto 0);
+    key: in std_logic_vector(127 downto 0);
+    data_out: out std_logic_vector(127 downto 0);
+    done: out std_logic
+  );
+end AES128;
+
+architecture Behavioural of AES128 is
+
+  -- signalen om componenten aan elkaar te hangen
+  signal key_out_ARK_in, ARK_out_SB_in, SB_out_shiftrow_in,
+         shiftrow_out_MC_in, MC_out_ARK_mux_in,
+         reg_out_ARK_in, ARK_mux_out_reg_in, DO_mux_out_reg_in,
+         final_data_out: STD_LOGIC_VECTOR(127 downto 0);
+
+  signal rcon_contr_rcon_keys: STD_LOGIC_VECTOR(3 downto 0);
+  signal contr_out_ARK_mux_sel, contr_out_DO_mux_sel: STD_LOGIC_VECTOR(1 downto 0);
+  signal done_sign: STD_LOGIC;
+
+  component Control_FSM is
+    port(
+    clock, reset, ce: in STD_LOGIC;
+    roundcounter: out STD_LOGIC_VECTOR(3 downto 0);
+    ARK_mux_sel, DO_mux_sel: out STD_LOGIC_VECTOR(1 downto 0);
+    done: out STD_LOGIC
+    );
+  end component;
+
+  component KeyScheduler is
+    port(
+      roundcounter: in STD_LOGIC_VECTOR(3 downto 0);
+		  clock: in std_logic;
+			reset: in std_logic;
+			ce: in std_logic;
+			key: in std_logic_vector(127 downto 0);
+			key_out: out std_logic_vector(127 downto 0)
+    );
+  end component;
+
+  component AddRoundKey is
+    port(
+      key_in: in std_logic_vector(127 downto 0);
+      text_in: in std_logic_vector(127 downto 0);
+      ARK_out: out std_logic_vector(127 downto 0)
+    );
+  end component;
+
+  component SubBytes is
+    port(
+      SB_in: in std_logic_vector( 127 downto 0 );
+      SB_out: out std_logic_vector( 127 downto 0 )
+    );
+  end component;
+
+  component ShiftRow is
+    port(
+      shiftrow_in: in std_logic_vector(127 downto 0);
+      shiftrow_out: out std_logic_vector(127 downto 0)
+    );
+  end component;
+
+  component MixColumn is
+    port(
+      MC_in: in std_logic_vector (127 downto 0);
+		  MC_out: out std_logic_vector(127 downto 0)
+    );
+  end component;
+
+begin
+
+  -- instantiaties van componenten
+  KeyS: KeyScheduler port map(rcon_contr_rcon_keys, clock, reset, ce, key,
+                            key_out_ARK_in);
+  Ctl_FSM: Control_FSM port map(clock, reset, ce, rcon_contr_rcon_keys,
+                           contr_out_ARK_mux_sel, contr_out_DO_mux_sel,
+                           done_sign);
+  ARK: AddRoundKey port map(key_out_ARK_in, reg_out_ARK_in, ARK_out_SB_in);
+  SB: SubBytes port map(ARK_out_SB_in, SB_out_shiftrow_in);
+  SR: ShiftRow port map(SB_out_shiftrow_in, shiftrow_out_MC_in);
+  MC: MixColumn port map(shiftrow_out_MC_in, MC_out_ARK_mux_in);
+
+  data_out <= final_data_out;
+  done <= done_sign;
+
+  -- ARK mux
+  ARK_mux: process(contr_out_ARK_mux_sel)
+  begin
+    case contr_out_ARK_mux_sel is
+      when "00" => ARK_mux_out_reg_in <= reg_out_ARK_in;
+      when "01" => ARK_mux_out_reg_in <= MC_out_ARK_mux_in;
+      when "11" => ARK_mux_out_reg_in <= data_in;
+      when others => ARK_mux_out_reg_in <= reg_out_ARK_in;
+    end case;
+  end process;
+
+  -- DataOut mux
+  DO_mux: process(contr_out_DO_mux_sel)
+  begin
+    case contr_out_DO_mux_sel is
+      when "00" => DO_mux_out_reg_in <= (others => '0');
+      when "01" => DO_mux_out_reg_in <= final_data_out;
+      when "11" => DO_mux_out_reg_in <= shiftrow_out_MC_in;
+      when others => DO_mux_out_reg_in <= (others => '0');
+    end case;
+  end process;
+
+  -- ARK_reg
+  ARK_reg: process(clock, reset)
+  begin
+    if reset = '1' then
+      reg_out_ARK_in <= (others => '0');
+    elsif rising_edge(clock) then
+      reg_out_ARK_in <= ARK_mux_out_reg_in;
+    end if;
+  end process;
+
+  -- DO_reg
+  DO_reg: process(clock, reset)
+  begin
+    if reset = '1' then
+      final_data_out <= (others => '0');
+    elsif rising_edge(clock) then
+      final_data_out <= DO_mux_out_reg_in;
+    end if;
+  end process;
+
+end Behavioural;
---
+++ Control_FSM.vhd
@@ -0,0 +1,167 @@
+library ieee;
+use ieee.STD_LOGIC_1164.ALL;
+use ieee.STD_LOGIC_ARITH.ALL;
+use ieee.STD_LOGIC_UNSIGNED.ALL;
+
+entity Control_FSM is
+  port(
+    clock, reset, ce: in STD_LOGIC;
+    roundcounter: out STD_LOGIC_VECTOR(3 downto 0);
+    ARK_mux_sel, DO_mux_sel: out STD_LOGIC_VECTOR(1 downto 0);
+    -- ARK_mux_sel
+      -- "11": de Plain_text wordt doorgelaten
+      -- "01": loopen
+      -- "00": waarde blijft behouden (er wordt niet geëncodeerd)
+    -- DO_mux_sel
+      -- "11": het resultaat van de encryptie gaat naar data_out
+      -- "01": data_out behoudt de waarde van na de encryptie
+      -- "00": data_out is 0.
+    done: out STD_LOGIC
+  );
+end Control_FSM;
+
+architecture Behavioural of Control_FSM is
+
+  type tStates is (sIdle, sFirstRound, sLoopUntil9, sLastRound, sDone);
+  -- Idle: de chip doet niets
+  -- sFirstRound: Plain_text wordt een eerste ronde geëncodeerd
+  -- LoopUntil9: data loopt negen keer door de verschillende codeerstappen
+  -- LastLoop: in de 10e ronde wordt de MixColumn stap overgeslagen
+  -- Done: de chip is klaar met de encryptie
+
+  signal curState, nxtState: tStates;
+  signal rcon_reg: STD_LOGIC_VECTOR(3 downto 0) := "0000";
+  signal done_reg, count_enable: STD_LOGIC;
+
+  begin
+    roundcounter <= rcon_reg;
+    done <= done_reg;
+
+-- Increment Counter
+  incr_ctr: process(clock, reset)
+  begin
+    if reset = '1' then
+      rcon_reg <= "0000";
+    elsif rising_edge(clock) then
+      if ce = '1' and count_enable = '1' then
+        if rcon_reg = "1010" then
+          rcon_reg <= "0000";
+        else
+          rcon_reg <= rcon_reg + 1;
+        end if;
+      else
+        rcon_reg <= rcon_reg;
+      end if;
+    end if;
+  end process;
+
+-- State Register
+  FSM_switchstate : process(clock, reset)
+  begin
+    if reset = '1' then
+      curState <= sIdle;
+    elsif rising_edge(clock) then
+      curState <= nxtState;
+    end if;
+  end process;
+
+--Next State Register
+  FSM_nxtState : process(curState, ce, rcon_reg, reset)
+  begin
+    case curState is
+        when sIdle =>
+        count_enable <= '0';
+          if reset = '1' then
+            nxtState <= sIdle;
+          else
+            if ce = '1' then
+              if rcon_reg = "0000" then
+                nxtState <= sFirstRound;
+              end if;
+            else
+              nxtState <= curState;
+            end if;
+          end if;
+
+        when sFirstRound =>
+        count_enable <= '1';
+          if reset = '1' then
+            nxtState <= sIdle;
+          else
+            if ce = '1' then
+              if rcon_reg = "0001" then
+                nxtState <= sLoopUntil9;
+              end if;
+            else
+              nxtState <= curState;
+            end if;
+          end if;
+
+        when sLoopUntil9 =>
+        count_enable <= '1';
+          if reset = '1' then
+            nxtState <= sIdle;
+          else
+            if ce = '1' then
+              if rcon_reg = "1001" then
+                nxtState <= sLastRound;
+              end if;
+            else
+              nxtState <= curState;
+            end if;
+          end if;
+
+        when sLastRound =>
+        count_enable <= '1';
+          if reset = '1' then
+            nxtState <= sIdle;
+          else
+            if ce = '1' then
+              nxtState <= sDone;
+            else
+              nxtState <= curState;
+            end if;
+          end if;
+
+        when sDone =>
+        count_enable <= '0';
+          if reset = '1' then
+            nxtState <= sIdle;
+          else
+            if ce = '0' then
+              nxtState <= sIdle;
+            else
+              nxtState <= curState;
+            end if;
+          end if;
+
+        when others =>
+          if reset = '1' then
+            nxtState <= sIdle;
+          else
+            if ce = '1' then
+              nxtState <= sIdle;
+            else
+              nxtState <= curState;
+            end if;
+          end if;
+    end case;
+  end process;
+
+  -- Output Function
+  Control_out: process(curState)
+  begin
+    case curState is
+      when sIdle => DO_mux_sel <= "00"; ARK_mux_sel <= "00"; done_reg <= '0';
+
+      when sFirstRound => DO_mux_sel <= "00"; ARK_mux_sel <= "11"; done_reg <= '0';
+
+      when sLoopUntil9 => DO_mux_sel <= "00"; ARK_mux_sel <= "01"; done_reg <= '0';
+
+      when sLastRound => DO_mux_sel <= "11"; ARK_mux_sel <= "01"; done_reg <= '0';
+
+      when sDone => DO_mux_sel <= "01"; ARK_mux_sel <= "01"; done_reg <= '1';
+    end case;
+  end process;
+
+end Behavioural;
