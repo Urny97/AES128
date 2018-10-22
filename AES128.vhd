b2eaccd874d5a0b853078c0e48e2953e0fee21a1
Arne Backx <arnebackx@hotmail.com>
2018-10-22 12:29:44 +0200

    volgorde verandert en correct aantal rondes toegevoegd

--- AES128.vhd
+++ AES128.vhd
@@ -18,23 +18,22 @@ end AES128;
 architecture Behavioural of AES128 is
 
   -- signalen om componenten aan elkaar te hangen
-  signal key_out_ARK_in, ARK_out_SB_in, SB_out_shiftrow_in,
-         SR_out_MC_in, MC_out_reg_in, MC_out_reg_out,
-         ARK_mux_out_ARK_in, data_in_reg_out,
-         final_data_out, reg_out_ARK_mux_in,
-         SR_reg_out: STD_LOGIC_VECTOR(127 downto 0);
+  signal key_out_ARK_in, ARK_out_reg_in, SB_out_shiftrow_in,
+         SR_out_MC_in, ARK_mux_out_ARK_in, MC_out_ARK_mux_in,
+         final_data_out, reg_out_SB_in, DI_reg_out: STD_LOGIC_VECTOR(127 downto 0);
 
   signal rcon_contr_rcon_keys: STD_LOGIC_VECTOR(3 downto 0);
   signal contr_out_ARK_mux_sel: STD_LOGIC_VECTOR(1 downto 0);
   signal done_sign, clear_sign, hold_data_out_sign,
-         contr_out_DO_mux_sel: STD_LOGIC;
+         contr_out_DO_mux_sel, read_data_in_sign: STD_LOGIC;
 
   component Control_FSM is
     port(
     clock, reset, ce: in STD_LOGIC;
     roundcounter: out STD_LOGIC_VECTOR(3 downto 0);
     ARK_mux_sel: out STD_LOGIC_VECTOR(1 downto 0);
-    DO_mux_sel, done, clear, hold_data_out: out STD_LOGIC
+    DO_mux_sel, done, clear, hold_data_out,
+    read_data_in: out STD_LOGIC
     );
   end component;
 
@@ -85,65 +84,59 @@ begin
                             key_out_ARK_in);
   Ctl_FSM: Control_FSM port map(clock, reset, ce, rcon_contr_rcon_keys,
                            contr_out_ARK_mux_sel, contr_out_DO_mux_sel,
-                           done_sign, clear_sign, hold_data_out_sign);
-  ARK: AddRoundKey port map(key_out_ARK_in, ARK_mux_out_ARK_in, ARK_out_SB_in);
-  SB: SubBytes port map(ARK_out_SB_in, SB_out_shiftrow_in);
+                           done_sign, clear_sign, hold_data_out_sign,
+                           read_data_in_sign);
+  ARK: AddRoundKey port map(key_out_ARK_in, ARK_mux_out_ARK_in, ARK_out_reg_in);
+  SB: SubBytes port map(reg_out_SB_in, SB_out_shiftrow_in);
   SR: ShiftRow port map(SB_out_shiftrow_in, SR_out_MC_in);
-  MC: MixColumn port map(SR_out_MC_in, MC_out_reg_in);
+  MC: MixColumn port map(SR_out_MC_in, MC_out_ARK_mux_in);
 
   data_out <= final_data_out;
   done <= done_sign;
 
   -- ARK mux
-  ARK_mux: process(contr_out_ARK_mux_sel, reg_out_ARK_mux_in, MC_out_reg_out, data_in_reg_out)
+  ARK_mux: process(contr_out_ARK_mux_sel, DI_reg_out, MC_out_ARK_mux_in, SR_out_MC_in)
   begin
     case contr_out_ARK_mux_sel is
-      when "00" => ARK_mux_out_ARK_in <= reg_out_ARK_mux_in;
-      when "01" => ARK_mux_out_ARK_in <= MC_out_reg_out;
-      when "11" => ARK_mux_out_ARK_in <= data_in_reg_out;
-      when others => ARK_mux_out_ARK_in <= MC_out_reg_out;
+      when "00" => ARK_mux_out_ARK_in <= DI_reg_out;
+      when "01" => ARK_mux_out_ARK_in <= MC_out_ARK_mux_in;
+      when "11" => ARK_mux_out_ARK_in <= SR_out_MC_in;
+      when others => ARK_mux_out_ARK_in <= (others => '0');
     end case;
   end process;
 
   -- DataOut mux
-  DO_mux: process(contr_out_DO_mux_sel, SR_reg_out)
+  DO_mux: process(contr_out_DO_mux_sel, ARK_out_reg_in)
   begin
     case contr_out_DO_mux_sel is
       when '0' => final_data_out <= (others => '0');
-      when '1' => final_data_out <= SR_reg_out;
+      when '1' => final_data_out <= ARK_out_reg_in;
       when others => final_data_out <= (others => '0');
     end case;
   end process;
 
-  -- ARK_reg
-  ARK_reg: process(clock, reset, clear_sign, data_in,
-                   MC_out_reg_in, ARK_mux_out_ARK_in)
+  -- SB_reg
+  SB_reg: process(clock, reset, clear_sign, ARK_out_reg_in, hold_data_out_sign)
   begin
     if reset = '1' then
-      data_in_reg_out <= (others => '0');
-      MC_out_reg_out <= (others => '0');
-      reg_out_ARK_mux_in <= (others => '0');
+      reg_out_SB_in <= (others => '0');
     elsif rising_edge(clock) then
       if clear_sign = '1' then
-        data_in_reg_out <= (others => '0');
-        MC_out_reg_out <= (others => '0');
-        reg_out_ARK_mux_in <= (others => '0');
-      else
-        data_in_reg_out <= data_in;
-        MC_out_reg_out <= MC_out_reg_in;
-        reg_out_ARK_mux_in <= ARK_mux_out_ARK_in;
+        reg_out_SB_in <= (others => '0');
+      elsif hold_data_out_sign = '0' then
+        reg_out_SB_in <= ARK_out_reg_in;
       end if;
     end if;
   end process;
 
-  -- SR_reg
-  SR_reg: process(clock, reset, hold_data_out_sign)
+  --data_in_reg
+  DI_reg: process(clock, reset, read_data_in_sign)
   begin
     if reset = '1' then
-      SR_reg_out <= (others => '0');
+      DI_reg_out <= (others => '0');
     elsif rising_edge(clock) then
-      if hold_data_out_sign = '1' then
-        SR_reg_out <= SR_out_MC_in;
+      if read_data_in_sign = '1' then
+        DI_reg_out <= data_in;
       end if;
     end if;
   end process;
