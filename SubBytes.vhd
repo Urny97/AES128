7ada3f415742c0bab2c6af878b972681585f4c5e
Arne Backx <arnebackx@hotmail.com>
2018-11-12 11:38:33 +0100

    begonnen aan 32 bit implementatie

--- SubBytes.vhd
+++ SubBytes.vhd
@@ -5,8 +5,9 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity SubBytes is
   port(
-    SB_in: in STD_LOGIC_VECTOR(127 downto 0);
-    SB_out: out STD_LOGIC_VECTOR(127 downto 0)
+    SB_in: in STD_LOGIC_VECTOR(31 downto 0);
+    SB_out: out STD_LOGIC_VECTOR(31 downto 0) 
+    -- blok nodig aan de uitgang die kolommen weer samenvoegt. Is dit de moeite?
   );
 end SubBytes;
 
@@ -21,21 +22,9 @@ end component;
 
 begin
 --instantiatie van component
-C1 : ByteSub port map (SB_in(127 downto 120), SB_out(127 downto 120));
-C2 : ByteSub port map (SB_in(119 downto 112), SB_out(119 downto 112));
-C3 : ByteSub port map (SB_in(111 downto 104), SB_out(111 downto 104));
-C4 : ByteSub port map (SB_in(103 downto 96), SB_out(103 downto 96));
-C5 : ByteSub port map (SB_in(95 downto 88), SB_out(95 downto 88));
-C6 : ByteSub port map (SB_in(87 downto 80), SB_out(87 downto 80));
-C7 : ByteSub port map (SB_in(79 downto 72), SB_out(79 downto 72));
-C8 : ByteSub port map (SB_in(71 downto 64), SB_out(71 downto 64));
-C9 : ByteSub port map (SB_in(63 downto 56), SB_out(63 downto 56));
-C10 : ByteSub port map (SB_in(55 downto 48), SB_out(55 downto 48));
-C11 : ByteSub port map (SB_in(47 downto 40), SB_out(47 downto 40));
-C12 : ByteSub port map (SB_in(39 downto 32), SB_out(39 downto 32));
-C13 : ByteSub port map (SB_in(31 downto 24), SB_out(31 downto 24));
-C14 : ByteSub port map (SB_in(23 downto 16), SB_out(23 downto 16));
-C15 : ByteSub port map (SB_in(15 downto 8), SB_out(15 downto 8));
-C16 : ByteSub port map (SB_in(7 downto 0), SB_out(7 downto 0));
+C1 : ByteSub port map (SB_in(31 downto 24), SB_out(31 downto 24));
+C2 : ByteSub port map (SB_in(23 downto 16), SB_out(23 downto 16));
+C3 : ByteSub port map (SB_in(15 downto 8), SB_out(15 downto 8));
+C4 : ByteSub port map (SB_in(7 downto 0), SB_out(7 downto 0));
 
 end Behavioural;
--- mixcolumn.vhd
+++ mixcolumn.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity MixColumn is 
 	port (	MC_in : in std_logic_vector (127 downto 0);
-			MC_out : out std_logic_vector(127 downto 0)
+			MC_out : out std_logic_vector(31 downto 0)
 			);
 end entity;
 
@@ -22,7 +22,9 @@ architecture MixColumn_arch of MixColumn is
 	end MULT;
 
 begin
-  MC_out(127 downto 96) <= (MULT(MC_in(127 downto 120)) xor
+-- Er is nog een controle signaal nodig dat aangeeft aan welke kolom van de input we zitten.
+  --kolom 1
+  MC_out <= (MULT(MC_in(127 downto 120)) xor
                              (MULT(MC_in(119 downto 112)) xor
                               MC_in(119 downto 112)) xor
                              MC_in(111 downto 104) xor
@@ -42,7 +44,8 @@ begin
                              MC_in(119 downto 112) xor
                              MC_in(111 downto 104) xor
                              MULT(MC_in(103 downto 96)));
-  MC_out(95 downto 64) <= (MULT(MC_in(95 downto 88)) xor
+  --kolom 2
+  MC_out <= (MULT(MC_in(95 downto 88)) xor
                             (MULT(MC_in(87 downto 80))xor
                              MC_in(87 downto 80)) xor
                             MC_in(79 downto 72) xor
@@ -62,7 +65,8 @@ begin
                             MC_in(87 downto 80) xor
                             MC_in(79 downto 72) xor
                             MULT(MC_in(71 downto 64)));
-  MC_out(63 downto 32) <= (MULT(MC_in(63 downto 56)) xor
+  --kolom 3
+  MC_out <= (MULT(MC_in(63 downto 56)) xor
                             (MULT(MC_in(55 downto 48))xor
                              MC_in(55 downto 48)) xor
                             MC_in(47 downto 40) xor
@@ -82,7 +86,8 @@ begin
                             MC_in(55 downto 48) xor
                             MC_in(47 downto 40) xor
                             MULT(MC_in(39 downto 32)));
-  MC_out(31 downto 0) <= (MULT(MC_in(31 downto 24)) xor
+  -- kolom 4
+  MC_out <= (MULT(MC_in(31 downto 24)) xor
                            (MULT(MC_in(23 downto 16)) xor MC_in(23 downto 16)) xor
                            MC_in(15 downto 8) xor MC_in(7 downto 0)) &
                           (MC_in(31 downto 24) xor MULT(MC_in(23 downto 16)) xor
